2c9149e45d0f9c1a8210d773afc7dcc2bd4df44b
Shikamaruna <dhanushgiri962@gmail.com>
2024-12-21 01:15:04 +0530

    BMI Project Create

--- src/App.js
+++ src/App.js
@@ -1,24 +1,75 @@
-import logo from './logo.svg';
+import {useState} from 'react';
 import './App.css';
 
+
 function App() {
+  const [height, setHeight] = useState("");
+  const [weight, setWeight] = useState("");
+  const [bmi, setBmi] = useState(null);
+  const [bmiStatus, setBmiStatus] = useState("");
+  const [errorMessage, seterrorMessage] = useState("")
+
+  const calculateBmi = () =>{
+    const isValidHeight=/^\d+$/.test(height);
+    const isValidWeight=/^\d+$/.test(weight);
+    if( isValidHeight && isValidWeight){
+      const heightInMeters = height / 100;
+      const bmiValue = weight / (heightInMeters * heightInMeters);
+      setBmi(bmiValue.toFixed(2));
+    if(bmiValue < 18.5){
+      setBmiStatus("Underweight");
+    } else if (bmiValue >= 18.5 && bmiValue < 24.9){
+      setBmiStatus("Normalweight");
+    } else if (bmiValue >= 25 && bmiValue < 29.9) {
+      setBmiStatus("Over Weight");
+    } else {
+      setBmiStatus("Obese")
+    }
+    seterrorMessage("")
+    
+    } else{
+      setBmi(null)
+      setBmiStatus("")
+      seterrorMessage("Please enter valid numeric values for hight and weight.");
+    }
+  };
+  const clearAll = () => {
+    setBmi(null);
+    setBmiStatus("");
+    setHeight("");
+    setWeight("");
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="bmi-calculater">
+      <div className='box'>
+        
+      </div>
+      <div className='data'>
+        <h1>BMI Calculater</h1>
+      
+        {errorMessage && <p className='error'>{errorMessage}</p> }
+        <div className='input-container'>
+          <label htmlFor='height'>Hight (cm) :</label>
+          <input type='text' value={height} id='height' onChange={(e)=>setHeight(e.target.value)}/>
+        </div>
+        <div className='input-container'>
+          <label htmlFor='Weight'>Weight (kg) :</label>
+          <input type='text' value={weight} id='Weight' onChange={(e)=>setWeight(e.target.value)} />
+        </div>
+        <button onClick={calculateBmi}>Calculate BMI</button>
+        <button onClick={clearAll}>Clear</button>
+       {bmi!==null &&(
+         <div className='result'>
+         <p>Your BMI is : {bmi}</p>
+         <p>Status : {bmiStatus}</p>
+       </div>
+       )}
+      </div>
     </div>
+    </>
+    
   );
 }
 
